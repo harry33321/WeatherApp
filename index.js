9fc83e96e0a22116221f031f463c4d0210a78772
HarryLam <harry33321@gmail.com>
2022-03-24 00:36:08 +0800

    fix weather date bug

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const todayDate = document.querySelector("#todayDate")
 const todayIcon = document.querySelector("#todayIcon")
-const todayTemperature = document.querySelector("#todayTemperature")
+const todayTemp = document.querySelector("#todayTemp")
 const humidity = document.querySelector("#humidity")
 const uvindex = document.querySelector("#uvindex")
 const generalSituation = document.querySelector("#generalSituation")
 
-const week1 = document.querySelector('#week1')
-const week2 = document.querySelector('#week2')
-const week3 = document.querySelector('#week3')
-const week4 = document.querySelector('#week4')
+const week1Date = document.querySelector('#week1Date')
+const week2Date = document.querySelector('#week2Date')
+const week3Date = document.querySelector('#week3Date')
+const week4Date = document.querySelector('#week4Date')
 const week1Icon = document.querySelector('#week1Icon')
 const week2Icon = document.querySelector('#week2Icon')
 const week3Icon = document.querySelector('#week3Icon')
@@ -25,11 +25,6 @@ const week1ForecastWind = document.querySelector('#week1ForecastWind')
 const week2ForecastWind = document.querySelector('#week2ForecastWind')
 const week3ForecastWind = document.querySelector('#week3ForecastWind')
 const week4ForecastWind = document.querySelector('#week4ForecastWind')
-
-const week1Date = document.querySelector('#week1Date')
-const week2Date = document.querySelector('#week2Date')
-const week3Date = document.querySelector('#week3Date')
-const week4Date = document.querySelector('#week4Date')
 
 const date = new Date()
 const dayList = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
@@ -50,19 +45,18 @@ const getTodayWeather = async () => {
 getTodayWeather().then(data => {
     todayDate.innerHTML = `${date.getMonth() + 1}月${date.getDate()}日 ${dayList[date.getDay()]}`
     todayIcon.src = `./image/pic${data.icon}.png`
-    todayTemperature.innerHTML = `${data.temperature.data[1].value} °C`
+    todayTemp.innerHTML = `${data.temperature.data[1].value} °C`
     humidity.innerHTML = `相對濕度：${data.humidity.data[0].value}%`
     // uvindex.innerHTML = `紫外線指數：${data.uvindex.data[0].desc}`
 }).catch(err => console.log(err))
 
 getResult().then(data => {
-    console.log(data.weatherForecast)
     generalSituation.innerHTML = data.generalSituation
 
-    week1Date.innerHTML = `${date.getMonth() + 1}月${date.getDate() + 1}日 ${data.weatherForecast[0].week}`
-    week2Date.innerHTML = `${date.getMonth() + 1}月${date.getDate() + 2}日 ${data.weatherForecast[1].week}`
-    week3Date.innerHTML = `${date.getMonth() + 1}月${date.getDate() + 3}日 ${data.weatherForecast[2].week}`
-    week4Date.innerHTML = `${date.getMonth() + 1}月${date.getDate() + 4}日 ${data.weatherForecast[3].week}`
+    week1Date.innerHTML = `${data.weatherForecast[0].forecastDate.slice(4,6)}月${data.weatherForecast[0].forecastDate.slice(6)}日 ${data.weatherForecast[0].week}`
+    week2Date.innerHTML = `${data.weatherForecast[1].forecastDate.slice(4,6)}月${data.weatherForecast[1].forecastDate.slice(6)}日 ${data.weatherForecast[1].week}`
+    week3Date.innerHTML = `${data.weatherForecast[2].forecastDate.slice(4,6)}月${data.weatherForecast[2].forecastDate.slice(6)}日 ${data.weatherForecast[2].week}`
+    week4Date.innerHTML = `${data.weatherForecast[3].forecastDate.slice(4,6)}月${data.weatherForecast[3].forecastDate.slice(6)}日 ${data.weatherForecast[3].week}`
 
     week1Icon.src = `./image/pic${data.weatherForecast[0].ForecastIcon}.png`
     week2Icon.src = `./image/pic${data.weatherForecast[1].ForecastIcon}.png`
